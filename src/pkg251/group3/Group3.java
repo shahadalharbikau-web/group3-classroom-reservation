adc2311ef39d4044d319a536997f124e595dc02f
Raghad-Alzahrani <zalzahrani771@gmail.com>
2025-11-24 23:46:59 +0000

    test commit

--- src/pkg251/group3/Group3.java
+++ src/pkg251/group3/Group3.java
@@ -16,6 +16,8 @@ public class Group3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello team! This is my first commit.");
+
     }
     
 }
