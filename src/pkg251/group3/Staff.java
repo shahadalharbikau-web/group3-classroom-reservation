222251e6896779b34987f7a471c43d746572b6a1
Raghad-Alzahrani <zalzahrani771@gmail.com>
2025-11-25 03:10:16 +0000

    Added Staff class with basic attributes and constructor

---
+++ src/pkg251/group3/Staff.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Staff {
+
+    private String staffID;
+    private String name;
+
+    public Staff(String staffID, String name){
+        this.staffID = staffID;
+        this.name = name;
+    }
+
+    public String getStaffID(){
+        return staffID;
+    }
+
+    // search Classroom method 
+    
+}
