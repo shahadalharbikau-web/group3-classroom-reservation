ca3cc050efb27984490f139e023567327801593d
joud15 <jjooud.144@gmail.com>
2025-11-25 16:58:24 +0000

    Booking class

---
+++ src/pkg251/group3/Booking.java
@@ -0,0 +1,15 @@
+public class Booking{
+
+    private static int counter =1;
+    private int bookingID;
+    private int numberOfStudents;
+    private String roomNumber;
+    private String staffID;
+    private String day;
+    private String time;
+    private  String status; //aprrove or reject 
+
+
+
+
+}
