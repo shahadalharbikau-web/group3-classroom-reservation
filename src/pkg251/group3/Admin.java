4883c5dddd4c019d27924f28b3771356598cc81c
sadeemy04 <sdymalmalky38@gmail.com>
2025-11-25 16:52:38 +0000

    last half of the Admin class include two core functions (manage booking and view all bookings)

--- src/pkg251/group3/Admin.java
+++ src/pkg251/group3/Admin.java
@@ -64,8 +64,65 @@ public class Admin{
      System.out.println(b.getDetails());
 
    }
-   
+    //admin will choose ID of the booking he/she want to change the statues of 
+        System.out.println("Enter booking ID to manage ( or 0 to exit):");
+        int id= input.nextLine();
+        if(id == 0){
+            return;
+        }
+        //this method will search for the booking object required by its id
+        Booking selected=null;
+        for(Booking b:pending){
+            if(b.getBookingID()==id){
+                selected=b;
+                break;
+            }
+        }
+    //if selected object dosent exist thin it will notify admin    
+    if(selected==null){
+        System.out.println("Booking ID not found");
+        
+    }
+    //will take admin input of approve or regect the booking object
+    else{
+        System.out.print("if Approve enter (A) || Reject enter (R)?");
+        String choice=input.nextLine();
+        //change status of the selected booking object based on admin choice (approve)
+        if(choice.equals("A")||choice.equals("a")){
+            selected.approve();
+            System.out.println("Booking rejected");
+            
+        }
+        //change status of the selected booking object based on admin choice (approve)
+        else if(choice.equals(choice.equals("R")||choice.equals("r"))){
+          selected.reject();
+          System.out.println("Booking rejected");
+          
+        } 
+        //if admin entered string other than R or A 
+        else{
+            System.out.println("Invalid choice");
+          }
+    
+    }
+  }
+     //this method will loop through Bookings arraylist and displaye it to admin
+     public void viewAllBookings(ArrayList<Booking>bookings){
+      System.out.println("--- All Bookings ---");
+      //if booking array was empty it will display a meassage to admin
+      if(bookings.isEmpty()){
+          System.out.println("No bookings in the system");
+      }
+      //if booking arraylist is not empty print booking objects details
+      else{
+          for(Booking b:bookings){
+              System.out.println(b);
+          }
+      }
+     
+    
 
    }
+
 
   }
